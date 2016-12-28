7c811143524d877a3af031dfe085630a80efcb79
Hung BCP <hungbcp@asiantech.vn>
2016-12-28 17:21:06 +0700

    feat: AOT-enabled build

--- dist/meta.service.js
+++ dist/meta.service.js
@@ -1,22 +1,10 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-var core_1 = require("@angular/core");
-var platform_browser_1 = require("@angular/platform-browser");
-var router_1 = require("@angular/router");
-require("rxjs/add/operator/filter");
-require("rxjs/add/operator/map");
-var meta_module_1 = require("./meta.module");
+var core_1 = require('@angular/core');
+var platform_browser_1 = require('@angular/platform-browser');
+var router_1 = require('@angular/router');
+require('rxjs/add/operator/filter');
+require('rxjs/add/operator/map');
+var meta_module_1 = require('./meta.module');
 var isDefined = function (val) { return typeof val !== 'undefined'; };
 var MetaService = (function () {
     function MetaService(router, document, titleService, activatedRoute, metaConfig) {
@@ -99,13 +87,18 @@ var MetaService = (function () {
         }
         return this;
     };
+    MetaService.decorators = [
+        { type: core_1.Injectable },
+    ];
+    /** @nocollapse */
+    MetaService.ctorParameters = function () { return [
+        { type: router_1.Router, },
+        { type: undefined, decorators: [{ type: core_1.Inject, args: [platform_browser_1.DOCUMENT,] },] },
+        { type: platform_browser_1.Title, },
+        { type: router_1.ActivatedRoute, },
+        { type: undefined, decorators: [{ type: core_1.Inject, args: [meta_module_1.META_CONFIG,] },] },
+    ]; };
     return MetaService;
 }());
-MetaService = __decorate([
-    core_1.Injectable(),
-    __param(1, core_1.Inject(platform_browser_1.DOCUMENT)),
-    __param(4, core_1.Inject(meta_module_1.META_CONFIG)),
-    __metadata("design:paramtypes", [router_1.Router, Object, platform_browser_1.Title, router_1.ActivatedRoute, Object])
-], MetaService);
 exports.MetaService = MetaService;
 //# sourceMappingURL=meta.service.js.map
